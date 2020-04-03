7164476c54287ab1ac024d098a77efda07ecb450
Zsolt Fakras <github@farkas.land>
2020-04-03 20:19:15 +0100

    upload fixed

--- build_pip_package.sh
+++ build_pip_package.sh
@@ -1,6 +1,6 @@
-sudo pip3 install --upgrade pip setuptools wheel
-sudo pip3 install tqdm
-sudo pip3 install --user --upgrade twine
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo python3 -m pip install tqdm
+sudo python3 -m pip install --user --upgrade twine
 
 python3 setup.py bdist_wheel
 
@@ -8,3 +8,5 @@ pip3 install dist/raspi_view-0.1-py3-none-any.whl
 
 # do not forget to set up your .pypirc file
 python3 -m twine upload dist/*
+# or use
+# sudo /root/.local/bin/twine upload dist/*

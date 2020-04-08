71a63ef338d1a633fbd0980d56b3f7d508138f6e
Zsolt Fakras <github@farkas.land>
2020-04-08 20:36:42 +0100

    cpu load chart added

--- main/views/load_view.py
+++ main/views/load_view.py
@@ -12,17 +12,33 @@ class LoadView(View):
         self.cpu_load = LoadAverage()
         self.disk_usage = DiskUsage()
 
-        self.status_cache = []
-        self.global_status = False
+        self.cpu_load_cache = []
         self.last_check = 0
 
     def get_warning_count(self):
+        cpu_load_in_percent = self.cpu_load.load_average / 4.0 * 100
+        if abs(self.last_check - time.time()) > 5.0:
+            self.cpu_load_cache.append(cpu_load_in_percent)
+            self.last_check = time.time()
+
+        if len(self.cpu_load_cache) > 128:
+            self.cpu_load_cache.pop(0)
+
         return (0, 0)
 
     def update_display(self, canvas, bounding_box):
-        canvas.text((0,0), "CPU Load: %.2f %%" % (self.cpu_load.load_average / 4.0 * 100) , fill="white")
+        if len(self.cpu_load_cache) > 0:
+            current_cpu_load = self.cpu_load_cache[-1]
+        else:
+            current_cpu_load = 0
+
+        canvas.text((0,0), "CPU Load: %.2f %%" % (current_cpu_load) , fill="white")
         canvas.text((0,10), "CPU Temp: %.2f °C" % self.cpu_temperature.temperature , fill="white")
         canvas.text((0,20), "Disk Usage: %.2f %%" % self.disk_usage.usage, fill="white")
+
+        for index, load in enumerate(self.cpu_load_cache):
+            canvas.line([(index, 63 - int(load/100.0 * 34.0)), (index, 63)], width=1, fill="white")
+
 
     def get_name(self) -> str:
         return "Load View"

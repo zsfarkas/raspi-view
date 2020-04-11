10dac61761bb140bdb766b2ce286b69aa327ef34
Zsolt Fakras <github@farkas.land>
2020-04-11 20:57:08 +0100

    string content fixed

--- main/views/network_status_view.py
+++ main/views/network_status_view.py
@@ -75,7 +75,7 @@ class NetworkStatusView(View):
         try:
             if NetworkStatusView._get_network_interface_state(interface) == 'down':
                 return None
-            cmd = "ifconfig %s | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'" % interface
+            cmd = "ifconfig %s | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'" % interface
             return subprocess.check_output(cmd, shell=True).decode('ascii')[:-1]
         except:
             return None 
